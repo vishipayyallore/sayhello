dda7c0c71f3122746a6ed163e1be4c9e8d6d987b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-14 17:54:54 +0530

    Initial Version

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const sayHello = function (name) {
+    const message = `${new Date().toUTCString()} :: Hello, ${name}`;
+
+    console.log(message);
+    return message;
+}
+
+module.exports = { sayHello };
